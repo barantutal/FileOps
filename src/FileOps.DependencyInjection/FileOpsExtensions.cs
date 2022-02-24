710a60ee9730f97e4d2490864fe23af6d908789f
Baran Tutal <info@freelanceryazilimci.net>
2022-02-24 20:28:03 +0300

    Add dependency injection

---
+++ src/FileOps.DependencyInjection/FileOpsExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace FileOps.DependencyInjection
+{
+    public static class FileOpsExtensions
+    {
+        public static IServiceCollection AddFileStore(this IServiceCollection services)
+        {
+            return services.AddFileOps(null);
+        }
+
+        public static IServiceCollection AddFileOps(this IServiceCollection services, Action<FileOpsOptions>? configure)
+        {
+            var fileStoreOptions = new FileOpsOptions("/");
+            configure?.Invoke(fileStoreOptions);
+
+            services.TryAddTransient<IFileOpsManager>(x => ActivatorUtilities.CreateInstance<FileOpsManager>(x, fileStoreOptions));
+            return services;
+        }
+    }
+}
