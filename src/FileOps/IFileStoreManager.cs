e5cd6f9f01e81cd071e0eee584846d591c9f3a2e
Baran Tutal <info@freelanceryazilimci.net>
2022-02-26 10:49:03 +0300

    Remove copyfile with guid

--- src/FileOps/IFileStoreManager.cs
+++ src/FileOps/IFileStoreManager.cs
@@ -8,6 +8,5 @@ public interface IFileOpsManager
     public void MoveDirectory(string sourcePath, string destinationPath);
     public IFileInfo GenerateFile(string path, byte[] content);
     public IFileInfo CopyFile(string path, string pathToCopy);
-    public IFileInfo CopyFile(Guid id, string pathToCopy);
     public void DeleteFile(string path);
 }
