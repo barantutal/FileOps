1be0e250ee7d9cee0606974e4899a441a04ed607
Baran Tutal <info@freelanceryazilimci.net>
2022-02-26 15:45:41 +0300

    Add test project

---
+++ src/FileOps.Tests.xUnit/FileTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Transactions;
+using Xunit;
+
+namespace FileOps.Tests.xUnit;
+
+public class FileTests : IClassFixture<FileOpsManager>
+{
+    private readonly FileOpsManager _fileOpsManager;
+    private readonly string _tempPath;
+    
+    public FileTests(FileOpsManager fileOpsManager)
+    {
+        _fileOpsManager = fileOpsManager;
+        _tempPath = Path.Combine(Path.GetTempPath(), "FileOpsManager-tempf-tests");
+        
+        if(Directory.Exists(_tempPath))
+        {
+            Directory.Delete(_tempPath, true);
+        }
+        
+        Directory.CreateDirectory(_tempPath);
+    }
+    
+    [Fact]
+    public void GeneratesFile()
+    {
+        var transactionScope = new TransactionScope();
+
+        var path = GenerateFilePath();
+        var content = GenerateContent();
+        _fileOpsManager.GenerateFile(path, content);
+        transactionScope.Complete();
+        transactionScope.Dispose();
+        
+        Assert.True(File.Exists(path));
+    }
+
+    private string GenerateFilePath()
+    {
+        return Path.Combine(_tempPath, $"{Guid.NewGuid()}.txt");
+    }
+
+    private byte[] GenerateContent()
+    {
+        return Encoding.UTF8.GetBytes(Guid.NewGuid().ToString());
+    }
+}
