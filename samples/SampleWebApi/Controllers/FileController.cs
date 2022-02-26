98d128c053e47ff67b844b8cb3f331d4140f5f2a
Baran Tutal <info@freelanceryazilimci.net>
2022-02-26 11:33:35 +0300

    Rename samples

--- samples/SampleWebApi/Controllers/FileController.cs
+++ samples/SampleWebApi/Controllers/FileController.cs
@@ -27,14 +27,14 @@ public class FileController : ControllerBase
             var info1 = Guid.NewGuid().ToString();
             var info2 = Guid.NewGuid().ToString();
 
-            var file1 = _fileOpsManager.GenerateFile($"/Users/barantutal/Desktop/test1/file-{info1}.txt", Encoding.UTF8.GetBytes(info1));
-            var file2 = _fileOpsManager.GenerateFile($"/Users/barantutal/Desktop/test1/file-{info2}.txt", Encoding.UTF8.GetBytes(info2));
+            var file1 = _fileOpsManager.GenerateFile($"/somePath/test1/file-{info1}.txt", Encoding.UTF8.GetBytes(info1));
+            var file2 = _fileOpsManager.GenerateFile($"/somePath/test1/file-{info2}.txt", Encoding.UTF8.GetBytes(info2));
                 
             responseModel.FileInfos.Add(file1);
             responseModel.FileInfos.Add(file2);
 
             // Content will be 'info1' for both files
-            var copiedFile = _fileOpsManager.CopyFile($"/Users/barantutal/Desktop/test1/file-{info1}.txt", $"/Users/barantutal/Desktop/test1/file-{info2}.txt");
+            var copiedFile = _fileOpsManager.CopyFile($"/somePath/test1/file-{info1}.txt", $"/somePath/test1/file-{info2}.txt");
             responseModel.FileInfos.Add(copiedFile);
                 
             if (throwEx == true)
